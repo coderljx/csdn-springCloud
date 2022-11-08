3ae4653d03203748b57ff4b406ba4b3ce8145a00
Brother <948917676@qq.com>
2022-11-08 21:56:16 +0800

    init

---
+++ bms_manger/src/main/java/com/example/bms_manger/BmsMangerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bms_manger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsMangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsMangerApplication.class, args);
+    }
+
+}
---
+++ bms_manger/src/test/java/com/example/bms_manger/BmsMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bms_manger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsMangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
